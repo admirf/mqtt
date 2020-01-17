5433c5d19c6e8caf9c4ef2f1980be50df798ce12
Admir Ferhatović <admir.ferhatovic@gmail.com>
2020-01-17 21:16:53 +0100

    Added paho tests

---
+++ paho/send.py
@@ -0,0 +1,30 @@
+import paho.mqtt.client as mqtt
+from datetime import datetime
+
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("test")
+
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic + ' ' + msg.payload)
+
+
+def main():
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.username_pw_set('ctnmhqle', 'KsDbKxfSoJmU')
+    client.connect("hairdresser.cloudmqtt.com", 18694)
+    client.publish('test', datetime.now())
+    # client.loop_forever()
+
+
+if __name__ == '__main__':
+    main()
